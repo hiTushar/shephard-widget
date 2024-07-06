1d3149cc663821b05d23e1a5cb86b9c1bf848b7b
Tushar H <tusharjain2201@gmail.com>
2024-07-06 18:56:51 +0530

    Ensure sectoral separation - every icon is in a unique sector

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -1,6 +1,7 @@
 import './Overview.css';
 import { appleSvg, androidSvg, windowsSvg, linuxSvg } from '../../assets/assets';
 import { useMemo } from 'react';
+import { inRadians } from '../../Utils/Utils';
 
 interface Props {
   data: Array<object>;
@@ -10,7 +11,9 @@ interface Props {
 const CENTER_CIRCLE_RADIUS_PERCENTAGE = 8;
 const LAST_ORBIT_RADIUS_PERCENTAGE = 45;
 const ORBIT_CENTER_OFFSET = 50;
-const ORBIT_EDGE_ICON_GAP = 5;
+const ORBIT_EDGE_ICON_GAP = 2;
+const PLOT_START_ANGLE = 0;
+const PLOT_END_ANGLE = 180;
 
 const PLATFORMS_ICON_MAP: object = {
   'macos': appleSvg,
@@ -29,6 +32,13 @@ const Overview: React.FC<Props> = ({ data, setSection }) => {
     return data.map((_, idx) => minRadius + radiusIncrement * (idx + 1));
   }, [data]);
 
+  const sectionAngleArray: Array<number> = useMemo(() => {
+    let minAngle = PLOT_START_ANGLE;
+    let maxAngle = PLOT_END_ANGLE;
+    let angleIncrement = (maxAngle - minAngle) / data.length;
+    return data.map((_, idx) => minAngle + angleIncrement * (idx)).concat(PLOT_END_ANGLE);
+  }, [data])
+
   const getSpokes: Array<JSX.Element> = (totalDivisions: number) => { 
     let spokeArray: Array<JSX.Element> = [];
     for (let idx = 0; idx <= totalDivisions; idx++) {
@@ -36,7 +46,7 @@ const Overview: React.FC<Props> = ({ data, setSection }) => {
         <div
           className="overview-center__spoke"
           key={idx}
-          style={{ transform: `rotate(-${180 / totalDivisions * idx}deg) ` }}
+          style={{ transform: `rotate(-${PLOT_END_ANGLE / totalDivisions * idx}deg)` }}
         >
         </div>
       )
@@ -75,8 +85,14 @@ const Overview: React.FC<Props> = ({ data, setSection }) => {
   const getPlatformIcons: Array<JSX.Element> = (data: Array<object>) => {
     return data.map((platform, idx) => {
       let orbitRadius = radiiArray[idx];
-      let xOffset = -orbitRadius + Math.random() * orbitRadius * 2;
-      console.log(xOffset);
+
+      let xPositionLowerbound = orbitRadius * Math.cos(inRadians(sectionAngleArray[idx + 1]));
+      let xPositionUpperbound = orbitRadius * Math.cos(inRadians(sectionAngleArray[idx]));
+      
+      let xOffset = xPositionLowerbound + Math.random() * (xPositionUpperbound - xPositionLowerbound);
+      console.log({idx, xPositionLowerbound, xPositionUpperbound, xOffset});
+
+
       if(xOffset + ORBIT_EDGE_ICON_GAP >= orbitRadius) {
         xOffset -= ORBIT_EDGE_ICON_GAP;
       }
@@ -102,6 +118,7 @@ const Overview: React.FC<Props> = ({ data, setSection }) => {
     })
   }
 
+  console.log(sectionAngleArray, radiiArray);
   return (
     <div className="overview">
       <div className="overview-center">
