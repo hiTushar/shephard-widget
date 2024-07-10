773d5f72ac69078d8ffd2d0f75397cd76e628b43
Tushar H <tusharjain2201@gmail.com>
2024-07-11 02:28:08 +0530

    Add logic to arrange data pts on the plot

--- src/components/Expanded/Expanded.tsx
+++ src/components/Expanded/Expanded.tsx
@@ -1,6 +1,7 @@
 import './Expanded.css';
 import { PLATFORMS_ICON_MAP } from '../../assets/assets';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
+import { inRadians } from '../../Utils/Utils';
 
 interface Props {
   data: Array<object>;
@@ -9,16 +10,19 @@ interface Props {
 
 const LEGEND_DATA:  Array<object> = [
   {
+    id: 'new_alerts',
     name: 'Assets with Alerts',
     desc: '',
     color: 'rgb(208, 6, 6)'
   },
   {
+    id: 'aged_alerts',
     name: 'Assets with Aged Alerts',
     desc: '> 14 days',
     color: 'rgb(125, 17, 17)'
   },
   {
+    id: 'other_assets',
     name: 'Other Assets',
     desc: '',
     color: 'rgb(15, 68, 92)'
@@ -29,8 +33,13 @@ const CENTER_CIRCLE_RADIUS_PERCENTAGE = 10;
 const LAST_ORBIT_RADIUS_PERCENTAGE = 65;
 const PLOT_START_ANGLE = 0;
 const PLOT_END_ANGLE = 360;
+const DIVISIONS = 8;
+const DATA_PT_MARGIN = 0.001; // percentage
+const ORBIT_CENTER_OFFSET_LEFT = 50;
+const ORBIT_CENTER_OFFSET_TOP = 52;
 
 const Expanded:React.FC<Props> = ({ data, section }) => {
+  const [dataPts, setDataPts] = useState([]);
 
   const sectionData = useMemo(() => {
     return data.find(platform => platform.platformId === section);
@@ -94,7 +103,7 @@ const Expanded:React.FC<Props> = ({ data, section }) => {
   const getSpokes: Array<JSX.Element> = (total: number) => {
     let spokeArray: Array<JSX.Element> = [];
     for (let idx = 0; idx < total; idx++) {
-      if(idx === 0 || idx === 4) continue
+      // if(idx === 0 || idx === 4) continue
       spokeArray.push(
         <div
           className="expanded-spokes__item"
@@ -107,8 +116,68 @@ const Expanded:React.FC<Props> = ({ data, section }) => {
     return spokeArray;
   }
 
+  const getDataPoints: Array<JSX.Element> = (sectionData: object) => { 
+    let containerSize = document.querySelector('.expanded').clientWidth;
+    let dataPtSize = 3 / 100 + 2 * DATA_PT_MARGIN;
+
+    let angleIncrementForAllOrbits = LEGEND_DATA.reduce((acc, dataType, idx) => {
+      let orbitRadius = radiiArray[idx];
+      let orbitSize = 2 * Math.PI * orbitRadius; // relative to the container
+      let sectionSize = orbitSize / sectionData.platformAssets[dataType.id].length;
+      sectionSize = sectionSize < dataPtSize ? dataPtSize : sectionSize;
+      return { ...acc,[dataType.id]: sectionSize / orbitSize * 360 };
+    }, {})
+    
+    console.log(containerSize, dataPtSize, angleIncrementForAllOrbits);
+
+    let minAngle = PLOT_START_ANGLE;
+    let maxAngle = PLOT_END_ANGLE;
+    let angleIncrementArrayForAllOrbits = Object.keys(angleIncrementForAllOrbits).reduce((acc, dataType, idx) => { 
+      let angleIncrement = angleIncrementForAllOrbits[dataType];
+      let angleIncrementArray = [];
+      for(let idx = 0; idx < sectionData.platformAssets[dataType].length; idx++) {
+        angleIncrementArray.push(minAngle + angleIncrement * idx);
+      }
+      return { ...acc, [dataType]: angleIncrementArray };
+    }, {})
+
+    console.log(angleIncrementArrayForAllOrbits);
+
+    let allAssets = sectionData.platformAssets;
+    let dataPtArray = Object.keys(allAssets).reduce((acc, dataType, idx) => {
+      let assets = allAssets[dataType]
+      let assetsDivArray = [];
+      for(let jdx = 0; jdx < assets.length; jdx++) {
+        let asset = assets[jdx];
+        let radialDistance = radiiArray[idx];
+        let angularPos = angleIncrementArrayForAllOrbits[dataType][jdx];
+        console.log(radialDistance, angularPos);
+
+        let xPos = radialDistance * Math.cos(inRadians(angularPos));
+        let yPos = radialDistance * Math.sin(inRadians(angularPos));
+        assetsDivArray.push(
+          <div 
+            className='expanded-dataPts__item'
+            data-asset-type={dataType} 
+            onMouseOver={() => {}}
+            key={`${dataType}${jdx}`}
+            style={{
+              left: `${ORBIT_CENTER_OFFSET_LEFT + xPos}%`,
+              top: `${ORBIT_CENTER_OFFSET_TOP + yPos}%`,
+            }}
+            onMouseEnter={() => {}}
+          >
+
+          </div>
+        )
+      }
+      return [...acc, ...assetsDivArray]
+    }, [])
+    setDataPts(dataPtArray);
+  }
+
   return (
-    <div className="expanded">
+    <div className="expanded" onLoad={() => getDataPoints(sectionData)}>
         <div className="expanded-orbit">
           <svg height="100%" width="100%" viewBox="0 0 100 100">
             <path 
@@ -131,11 +200,13 @@ const Expanded:React.FC<Props> = ({ data, section }) => {
           </div>
           <div className="expanded-system__spokes">
             {
-              getSpokes(8)
+              getSpokes(DIVISIONS)
             }
           </div>
           <div className="expanded-system__dataPts">
-
+            {
+              dataPts
+            }
           </div>
         </div>
         <div className="expanded-legend">
