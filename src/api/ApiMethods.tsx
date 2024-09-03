4b9692ac16815f258f92926c4cba0057d9cbd109
Ambuj Kumar <ambujs89@gmail.com>
2024-09-03 22:24:43 +0530

    fix: use res.json() to correctly process fetch response

--- src/api/ApiMethods.tsx
+++ src/api/ApiMethods.tsx
@@ -1,19 +1,23 @@
-const getAuthToken = () => { return '' }
+const getAuthToken = () => {
+  return "";
+};
 
 class ApiMethods {
-    static apiRequest<T>(url: string, method: string) {
-        return fetch(url, {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': getAuthToken()
-            }
-        }) as Promise<T>;
-    }
+  static async apiRequest<T>(url: string, method: string) {
+    const res = await fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: getAuthToken(),
+      },
+    });
 
-    static get<T>(url: string) {
-        return this.apiRequest<T>(url, 'GET');
-    }
+    return res.json() as Promise<T>;
+  }
+
+  static get<T>(url: string) {
+    return this.apiRequest<T>(url, "GET");
+  }
 }
 
 export default ApiMethods;
